1a15f12112c3a7fc3da539f1cbfdd667e5afe73f
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-04-20 21:37:58 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+attr_accessor :the_word 
+
+def initialize(a_word)
+@the_word = a_word 
+end 
+
+def match(the_list_of_words_i_guess)
+  x = @the_word.split("").sort
+  y= the_list_of_words_i_guess.split("").sort
+  x.detect?(y)
+end
+
+end
